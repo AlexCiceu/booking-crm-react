d99fcdf4d378fde276a05beb91a904f97e9771ff
Raluca Ghionea <raluca.ghionea@wecodepixels.com>
2019-11-28 16:24:25 +0200

    Added Material UI

--- assets/js/components/app.js
+++ assets/js/components/app.js
@@ -5,6 +5,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+import Select from "@material-ui/core/Select";
+import Input from "@material-ui/core/Input";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputLabel from "@material-ui/core/InputLabel";
 
 class App extends React.Component {
     constructor(props){
@@ -169,7 +175,8 @@ class App extends React.Component {
         }, () => {
             console.log('End date: ', this.state.selectedDateTo.toISOString().split('T')[0])
         })
-    }
+    };
+
 
      componentDidMount() {
         this.handleRetrieveCountries();
@@ -181,10 +188,76 @@ class App extends React.Component {
 
     render() {
         return(
-            <div className="col-md-12 border-primary ml-sm-1 mr-sm-1">
+            <div className="col-lg-12">
                 <div className="col-md-12 border-primary mb-md-5">
                     <h1 className="display-2 text-center">Travel Idea Booking</h1>
                 </div>
+
+
+                <div className="col-sm-12">
+                    <InputLabel id="demo-simple-select-label" className="col-md-6">Alegeti tara</InputLabel>
+                    <Select
+                        className="col-sm-3"
+                        labelId="demo-mutiple-name-label"
+                        id="demo-mutiple-name"
+                        onChange={this.handleCountriesSelectChange}
+                        input={<Input/>}>
+                        {
+                            this.state.countries.map(data => {
+                                return (
+                                    <MenuItem value={data}
+                                              key={data}>
+                                        {data}
+                                    </MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                </div>
+
+                <div className="col-sm-12">
+                    <InputLabel id="demo-simple-select-label" className="col-md-6">Alegeti locatia</InputLabel>
+                    <Select
+                        className="col-sm-3"
+                        labelId="demo-mutiple-name-label"
+                        id="demo-mutiple-name"
+                        onChange={this.handleLocationSelectChange}
+                        input={<Input/>}>
+                        {
+                            this.state.locations.map(data => {
+                                return (
+                                    <MenuItem value={data}
+                                              key={data}>
+                                        {data}
+                                    </MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                </div>
+
+                <div className="col-sm-12">
+                    <InputLabel id="demo-simple-select-label" className="col-md-6">Alegeti resortul</InputLabel>
+                    <Select
+                        className="col-sm-3"
+                        labelId="demo-mutiple-name-label"
+                        id="demo-mutiple-name"
+                        onChange={this.handleResortNameSelectChange}
+                        input={<Input/>}>
+                        {
+                            this.state.hotelNames.map(data => {
+                                return (
+                                    <MenuItem value={data}
+                                              key={data}>
+                                        {data}
+                                    </MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                </div>
+
+
                 <div className="row">
                     <div className="col-sm-6">
                         <div className="form-group">
@@ -229,7 +302,7 @@ class App extends React.Component {
                         <div className="form-group">
                             <select className="form-control" name="selectedResortName"
                                     onChange={this.handleResortNameSelectChange}>
-                                <option value="" selected disabled hidden>Alegeti resortul</option>
+                                <option value="" selected disabled hidden>Alegeti resortul TEST</option>
 
                                 {
                                     this.state.hotelNames.map(data => {
@@ -244,29 +317,29 @@ class App extends React.Component {
                             </select>
                         </div>
                     </div>
-                    <div className="col-sm-2">
-                        {
-                            <DatePicker
-                                selected={this.state.selectedDateFrom}
-                                onChange={this.handleStartDateOnChange}
-                            />
-                        }
-                    </div>
-                    <div className="col-sm-2">
-                        {
-                            <DatePicker
-                                selected={this.state.selectedDateTo}
-                                onChange={this.handleEndDateOnChange}
-                            />
-                        }
+                        <div className="col-sm-3">
+                            {
+                                <DatePicker
+                                    selected={this.state.selectedDateFrom}
+                                    onChange={this.handleStartDateOnChange}
+                                />
+                            }
+                        </div>
+                        <div className="col-sm-3">
+                            {
+                                <DatePicker
+                                    selected={this.state.selectedDateTo}
+                                    onChange={this.handleEndDateOnChange}
+                                />
+                            }
+                        </div>
                     </div>
                     <div className="col-sm-2">
                         <button className="btn btn-primary" type="submit"
                                 onClick={this.handleRetrieveAvailableBooking}>
-                            Find booking
+                            Booking
                         </button>
                     </div>
-                </div>
                 <div className="row">
                     {
                         this.state.isHotelInfoRetrieved ?
